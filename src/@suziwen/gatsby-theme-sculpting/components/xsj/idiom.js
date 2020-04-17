58ac5f24189a2bc319ed1af6c9cdf81755234313
suziwen <suziwen1@gmail.com>
2020-04-17 13:30:06 +0800

    icon

--- src/@suziwen/gatsby-theme-sculpting/components/xsj/idiom.js
+++ src/@suziwen/gatsby-theme-sculpting/components/xsj/idiom.js
@@ -1,7 +1,7 @@
 /** @jsx jsx */
 import { jsx } from 'theme-ui'
 import React from "react"
-import {GiConqueror} from "react-icons/gi"
+import {GiAcorn} from "react-icons/gi"
 import ContextConsumer from "@suziwen/gatsby-theme-sculpting/src/components/context"
 
 const XSJTitle = ()=> {
@@ -10,7 +10,7 @@ const XSJTitle = ()=> {
       {({siteMetadata: {idiom}}) => {
         return (
           <span sx={{fontFamily: "logo", fontWeight: 'normal'}}>
-            Time Flies, No Time for Nuts <GiConqueror  sx={{fontSize: '1.5em', verticalAlign: 'text-bottom'}}/>
+            Time Flies, No Time for Nuts <GiAcorn  sx={{fontSize: '1.5em', verticalAlign: 'text-bottom'}}/>
           </span>
         )
       }}
