7fcde8b71961e49eb8c7bb864137cc77b6c063b5
suziwen <suziwen1@gmail.com>
2020-04-28 20:36:39 +0800

    调整字体

--- src/gatsby-plugin-theme-ui/index.js
+++ src/gatsby-plugin-theme-ui/index.js
@@ -6,5 +6,7 @@ import merge from "lodash.merge"
 export default merge({}, baseTheme, {
   fonts: {
     logo: "aladin, sans-serif",
+    body: "'Noto Serif SC', system-ui, 'Helvetica Neue', Helvetica, Tahoma, Arial, 'Hiragino Sans GB', 'NSimSun', '仿宋体','SimSun', '宋体', serif",
+    heading: "'Noto Sans SC', 'Helvetica Neue', Helvetica, Tahoma, Arial, 'Hiragino Sans GB', 'Microsoft YaHei', '微软雅黑', 'WenQuanYi Micro Hei', Heiti, '黑体', sans-serif",
   },
 })
